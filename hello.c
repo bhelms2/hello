21af5abdd184c1c99044010d6637fe6f52038473
Bryce Helms <bhelms3@huskers.unl.edu>
2024-08-29 11:28:34 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Bryce Helms
+ * Date: 2024/08/27
+ *
+ * A simple Bryce Helms program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+    printf("Bryce Helms/n");
+
+    return 0;
+}
+
